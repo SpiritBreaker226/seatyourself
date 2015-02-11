9b39a62c8412512347fcb5ae3e9c6807087e57f9
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-11 18:57:43 -0500

    Add Session Routes To Login, Create & Destroy

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  get 'login' => 'sessions/new', as: :login
+  resources :sessions, only: [:create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
