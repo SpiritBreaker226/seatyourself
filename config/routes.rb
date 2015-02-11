e2b9db7d3ad0865e5ebcf00f109ba0658dcfa0c0
CivicImages <kobewka@gmail.com>
2015-02-11 15:54:51 -0500

    Adding reservations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+
+  get 'reservations/show'
+
+  get 'reservations/create'
+
+  get 'reservations/edit'
+
+  get 'reservations/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
