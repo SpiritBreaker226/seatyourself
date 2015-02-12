d0f16723dbfa9fe349255e18dd70972fe815f286
CivicImages <kobewka@gmail.com>
2015-02-12 16:18:39 -0500

    Adding methods for each route

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,49 @@
+class ReservationsController < ApplicationController
+  def index
+    @reservations = Reservations.all
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      redirect_to restaurant_url(params[:id]), notice: "You've created a reservation!"
+    else
+      flash.now[:alert] = "Something went wrong."
+      render :new
+    end
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+
+    if @reservation.update_attributes(restaurant_params)
+      redirect_to restaurants_path(@restaurant)
+    else
+      render :edit
+    end    
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    reservation.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:time, :restaurant_id, :user_id, :party_size)
+  end
+end
