89d4c582fb35182284ac3ead952a6d8fa733ab0c
emilyjfan <emilyjustinefan@gmail.com>
2015-02-12 18:11:22 -0500

    Adding validations and flash notices and alerts to creeate restaurant view.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,8 +19,9 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new(restaurant_params)
 
     if @restaurant.save
-      redirect_to restaurants_path 
+      redirect_to restaurants_path, :notice => "Restaurant created!"   
     else
+      flash.now[:alert] = "Could not save! Ensure all fields are completed."
       render :new
     end
   end
