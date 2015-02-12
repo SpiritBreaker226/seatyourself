e5a8ac59c71572b0c5992b0438ce8766031b2968
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-11 19:44:57 -0500

    Fix Routing To Login Point To Wrong Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  get 'login' => 'sessions/new', as: :login
   resources :sessions, only: [:create, :destroy]
+  get 'login' => 'sessions#new', as: :login
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
