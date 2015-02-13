b585f5bfb5422343fdb4aa798e6eca2c124f49ff
emilyjfan <emilyjustinefan@gmail.com>
2015-02-13 12:09:58 -0500

    creating available? method to the Restaurant class

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,10 @@
 class Restaurant < ActiveRecord::Base
 	validates :name, :address, :description, :capacity, :hours_open, :hours_closed, presence: true 
 	has_many :reservations
+
+	def available? (party_size, time)
+		reservation_count = reservations.sum(:party_size)
+		party_size + reservation_count <= capacity 
+	end
+
 end
