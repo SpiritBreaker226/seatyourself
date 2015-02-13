84fbe0a47f1621622f35e0dbb6778e98b57120a7
emilyjfan <emilyjustinefan@gmail.com>
2015-02-13 16:05:51 -0500

    adding validations to reservation class.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,16 @@
 class Reservation < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :user
+
+  validates :party_size, :time, presence: true 
+
+  validate :availability 
+
+  private
+  	def availability 
+  		if !restaurant.available?(party_size, time)
+  			errors.add(:base, "Restaurant is full. Please try a different time.")
+  		end
+  	end
+
 end
