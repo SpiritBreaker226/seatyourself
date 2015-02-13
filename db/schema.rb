f4443bc0ea1925e2baff7edec08dac68a1ee2dec
emilyjfan <emilyjustinefan@gmail.com>
2015-02-12 19:38:45 -0500

    Creating seeds for Restaurant, User, and Reservation models. Updated migrate file.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211202921) do
+ActiveRecord::Schema.define(version: 20150211195931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,4 +48,6 @@ ActiveRecord::Schema.define(version: 20150211202921) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "reservations", "restaurants"
+  add_foreign_key "reservations", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,71 @@
+User.create!(
+  :first_name => "Martha",
+  :last_name => "Stewart",
+  :email => "martha@stewart.com",
+  :password => "password",
+)
+
+User.create!(
+  :first_name => "Michael",
+  :last_name => "Jackson",
+  :email => "michaeljackson@email.com",
+  :password => "password",
+)
+
+User.create!(
+  :first_name => "Your",
+  :last_name => "Mom",
+  :email => "mommy@email.com",
+  :password => "password",
+)
+
 Restaurant.create!(
   	:name => "Banjara",
   	:address => "796 Bloor Street West, Toronto, ON M6G 1L7",
   	:description => "Popular dining option offering North & South Indian fare, with beer on tap plus takeout & delivery.",
   	:capacity => 50,
   	:hours_open => Time.strptime("17:00:00", "%H:%M:%S"),
-  	:hours_closed => Time.strptime("17:00:00", "%H:%M:%S"),
+  	:hours_closed => Time.strptime("23:00:00", "%H:%M:%S"),
+)
+
+Restaurant.create!(
+  	:name => "Little Nicky's",
+  	:address => "375 Queen St W, Toronto, ON M5V 2A5",
+  	:description => "Cozy cafe with a vintage vibe that sells espresso drinks & beans, plus housemade sugary mini-donuts.",
+  	:capacity => 20,
+  	:hours_open => Time.strptime("16:00:00", "%H:%M:%S"),
+  	:hours_closed => Time.strptime("22:00:00", "%H:%M:%S"),
+)
+
+Restaurant.create!(
+  	:name => "The Keg",
+  	:address => "165 York Street, Toronto, Ontario M5H 3R8",
+  	:description => "Quality, comfort and value are three key ingredients that make The Keg Steakhouse & Bar experience like no other.",
+  	:capacity => 100,
+  	:hours_open => Time.strptime("12:00:00", "%H:%M:%S"),
+  	:hours_closed => Time.strptime("23:00:00", "%H:%M:%S"),
 )
 
 Reservation.create!(
 	:time => DateTime.strptime('2001-02-03T04:05:06+07:00', '%Y-%m-%dT%H:%M:%S%z'),
-	:restaurant_id => 1,
-	:user_id => 1,
+	:restaurant => Restaurant.first,
+	:user => User.first,
 	:party_size => 2,
 )
 
-User.create!(
-  :first_name => "Martha",
-  :last_name => "Stewart",
-  :email => "martha@steware.com",
-  :password => "password",
+Reservation.create!(
+	:time => DateTime.strptime('2001-02-03T04:05:06+07:00', '%Y-%m-%dT%H:%M:%S%z'),
+	:restaurant => Restaurant.second,
+	:user => User.second,
+	:party_size => 4,
 )
 
+Reservation.create!(
+	:time => DateTime.strptime('2001-02-03T04:05:06+07:00', '%Y-%m-%dT%H:%M:%S%z'),
+	:restaurant => Restaurant.third,
+	:user => User.third,
+	:party_size => 10,
+)
 
 
 
