45eca61e357cef527d8cb067e5a714404c335816
CivicImages <kobewka@gmail.com>
2015-02-12 21:50:10 -0500

    Added show restaurant reservations. Converted buttons on restaurant show page to links with button class.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,6 +2,10 @@ class ReservationsController < ApplicationController
   
   before_filter :find_restaurant
 
+  def index
+    @reservations = @restaurant.reservations
+  end
+
   def edit
     @reservation = Reservation.find(params[:id])
   end
@@ -19,21 +23,22 @@ class ReservationsController < ApplicationController
     end
   end
 
-  def update
-    @reservation = Reservation.find(params[:id])
+  # To Do: Create user edit view for reservations
+  # def update
+  #   @reservation = Reservation.find(params[:id])
 
-    if @reservation.update_attributes(restaurant_params)
-      redirect_to restaurants_path(@restaurant)
-    else
-      render :edit
-    end    
-  end
+  #   if @reservation.update_attributes(restaurant_params)
+  #     redirect_to restaurants_path(@restaurant)
+  #   else
+  #     render :edit
+  #   end    
+  # end
 
-  def destroy
-    @reservation = Reservation.find(params[:id])
-    reservation.destroy
-    redirect_to restaurants_path
-  end
+  # def destroy
+  #   @reservation = Reservation.find(params[:id])
+  #   reservation.destroy
+  #   redirect_to restaurants_path
+  # end
 
   private
   def reservation_params
