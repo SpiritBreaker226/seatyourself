69c2546748f14a5000407d7d4584ddfdaed60098
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-13 15:59:26 -0500

    Enforce Logged In For Create, Update & Delete Item
    Enforce logged in in order for guest of the site not to create, update
    and delete restaurant, reservation. Also not allow anyone to edit a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,13 @@ class ApplicationController < ActionController::Base
   def current_user
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  
-  helper_method :current_user
+
+  def ensure_logged_in
+  	unless current_user
+  		flash[:alert] = "Please log in"
+  		redirect_to login_path
+  	end
+  end
+    
+  helper_method :current_user, :ensure_logged_in
 end
